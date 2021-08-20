e942bd9d41ff57a84d0141492246eaf3e66683aa
Bhuwan Paneru <26891567+bhuwanpaneru@users.noreply.github.com>
2021-08-20 13:06:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,7 @@
 #  required_version = ">= 0.12, < 0.13"
 #}
 
-#subscription_id= var.subscriptionID
-#client_id       = "${var.client_id}"
-#client_secret   = "${var.client_secret}"
-#tenant_id       = "${var.tenant_id}"
+
 
 terraform {
   required_providers {
