4152ae2c3e5a39e9aa778af95dd310d3636116f9
Bhuwan Paneru <26891567+bhuwanpaneru@users.noreply.github.com>
2021-08-23 17:44:15 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-eval "$(jq -r '@sh "GROUP_NAME=\(.group_name)"')"
+eval "$(jq -r '@sh "GROUP_NAME=\(.rgName)"')"
 result=$(az group exists -n $GROUP_NAME)
 
 jq -n --arg exists "$result" '{"exists":$exists}'
