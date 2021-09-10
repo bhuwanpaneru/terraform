53548860208867939f2208a07230d32225e39469
Bhuwan Paneru <26891567+bhuwanpaneru@users.noreply.github.com>
2021-09-10 15:06:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,9 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
-
-
-provider "azurerm" {
-  features {}
-    subscription_id = "${var.subscription_id}"
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-    tenant_id       = var.tenant_id
-}
-
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.rgName}"
-  location = "var.location"
-
-  tags = {
-    environment = "Dev"
-  }
-}
-
-
-
-
-
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 
 #terraform {
 #  required_version = ">= 0.12, < 0.13"
 #}
-
-
 
 terraform {
   required_providers {
@@ -52,7 +18,6 @@ terraform {
 # version = "~> 1.34.0"
 #}
 
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
@@ -66,7 +31,7 @@ provider "azurerm" {
 # Hub RG
 resource "azurerm_resource_group" "rg" {
   name     = rgName
-  location = "var.location"
+  location = var.location
   tags = {
     environment = "Dev"
   }
