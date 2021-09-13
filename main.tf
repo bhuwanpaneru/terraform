e0c3273e34ce93d6e353c84cf2ebaf8ec95bca33
Bhuwan Paneru <26891567+bhuwanpaneru@users.noreply.github.com>
2021-09-13 12:26:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm" 
-      version = "~>1.0.1"
+      version = "~>2.5.0"
    }
   }
   #required_version = ">= 0.12, < 0.13"
