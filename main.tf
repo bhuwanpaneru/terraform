74b2a7f4935a296888d89c938a79dba9a1b434d7
Bhuwan Paneru <26891567+bhuwanpaneru@users.noreply.github.com>
2021-09-13 12:14:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,13 +20,15 @@
 #}
 
 # Configure the Microsoft Azure Provider
-provider "azurerm" {
-  version = "=2.75.0"
+terraform {
+  provider "azurerm" {
+  version = "~>2.1.0"
   features {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+ }
 }
 
 # Hub RG
